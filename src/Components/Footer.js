42834a52615b479a20b98d9062954c74ac2a600d
joshli52 <67028612+joshli52@users.noreply.github.com>
2023-03-04 16:06:46 -0500

    added navbar

---
+++ src/Components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import '../styles/footer.css';
+
+function Footer() {
+  return ( 
+    <div className = "footer" > 
+    <div className ="socialMedia" >
+        <GitHubIcon/>
+        <LinkedInIcon/>
+        <EmailIcon/>
+    </div>
+    <p> &copy; 2022 joshuali.dev </p>
+    </div>
+  );
+}
+
+export default Footer
+
+

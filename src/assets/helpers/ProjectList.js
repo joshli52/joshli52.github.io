e87a11732ceadc62374460f2a7d9aa7c1d9f1882
joshli52 <67028612+joshli52@users.noreply.github.com>
2023-03-04 23:42:13 -0500

    added css to projectdisplay

---
+++ src/assets/helpers/ProjectList.js
@@ -0,0 +1,14 @@
+import forest from "../forest.jpg";
+// this file serves as a way to easily change as manipulate projects listed in "../pages/projects"
+// this file is refernced by "../pages/ProjectDisplay for the image and name inputs
+export const ProjectList = [
+  {
+    name: "forest",
+    image: forest,
+    skills: "Django, API, Python",
+  },
+  { name: "exampleproject2",
+    image: forest,
+    skills: "React, OOP, Javascript",
+  },
+]
